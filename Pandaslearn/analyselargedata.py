00ef51f8ec8b3e86f940393cc6e2dd02ab5130d8
WindJammer6 <98175995+WindJammer6@users.noreply.github.com>
2023-02-21 00:08:16 +0800

    Delete Pandaslearn directory

--- Pandaslearn/analyselargedata.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-#Chunksize refers to number of rows in the file to select at once
-#Printed a "CHUNK DATAFRAME" to indicate the different chunks of 5 being printed
-for updated_poke_dataset_csv in pd.read_csv('updated_pokemon_data.csv', chunksize=5):
-    print("CHUNK DATAFRAME")
-    print(updated_poke_dataset_csv)
